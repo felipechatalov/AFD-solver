b04b5052ca55fbedd2d06190aa9d01c5e0aefb3b
Felipe Chatalov <fechatalov@gmail.com>
2024-01-12 01:07:52 -0300

    Updating, still need additions

--- interface.py
+++ interface.py
@@ -18,11 +18,12 @@ class Interface():
         #window = tk.Toplevel(master)
         self.command_state = "None"
         self.circles_holder = []
-        self.trans_holder = []
+        self.transitions_holder = []
         self.image_holder = None
         self.master = master
         self.master.title("Interface")
         self.master.geometry(f"{MAX_WIDTH}x{MAX_HEIGHT}")
+        self.aux_add_transition = None
         self._WIDTH = MAX_WIDTH
         self._HEIGHT = MAX_HEIGHT
         self.create_buttons()
@@ -48,6 +49,7 @@ class Interface():
     def is_inside_circle(self, x, y, circle):
         x0, y0, r = circle
         return (x-x0)**2 + (y-y0)**2 <= r**2
+    
     def is_inside_any_circle(self, x, y, circles):
         index = 0
         for circle in circles:
@@ -79,15 +81,93 @@ class Interface():
                 
                 # redraw circles
                 self.redraw_circles(self.circles_holder)
-                pass
-            pass
         elif self.command_state == "AddTransition":
-            pass
+
+            if self.aux_add_transition is None:
+                self.aux_add_transition = self.is_inside_any_circle(event.x, event.y, self.circles_holder)
+                print(f"aux_add_transition set to {self.aux_add_transition}")
+            else:
+                aux = self.is_inside_any_circle(event.x, event.y, self.circles_holder)
+                print(f"aux is {aux}")
+                if self.aux_add_transition is not None and aux is not None:
+                    print(f"adding transition from S{self.aux_add_transition} to S{aux}")
+                    
+                    self.add_transition(self.aux_add_transition, aux)
+                
         elif self.command_state == "RmTransition":
             pass
         else:
             pass
         return        
+
+    def add_transition(self, t1, t2):
+        sec_window = tk.Toplevel(self.master)
+        sec_window.title("Transition")
+        sec_window.geometry("300x100")
+        entry = tk.Entry(sec_window)
+        entry.pack()
+
+        def add_text_and_close():
+            values = entry.get()
+
+            if self.is_any_transition_with(t1, t2, values):
+                print("existing transition")
+                return
+
+            if len(values) != 3 or values[2] not in ["d", "D", "e", "E"]:
+                print("invalid input")
+                return
+
+            t1x, t1y, t1r = self.circles_holder[t1]
+            t2x, t2y, t2r = self.circles_holder[t2]
+
+            offsetx1 = t1r * (t2x - t1x) / ((t2x - t1x)**2 + (t2y - t1y)**2)**0.5
+            offsety1 = t1r * (t2y - t1y) / ((t2x - t1x)**2 + (t2y - t1y)**2)**0.5
+
+            offsetx2 = t2r * (t1x - t2x) / ((t2x - t1x)**2 + (t2y - t1y)**2)**0.5
+            offsety2 = t2r * (t1y - t2y) / ((t2x - t1x)**2 + (t2y - t1y)**2)**0.5
+
+            t1x += offsetx1
+            t1y += offsety1
+
+            t2x += offsetx2
+            t2y += offsety2
+
+            pmedio = ((t1x + t2x)/2, (t1y + t2y)/2)
+
+            vetor = ((t2x - t1x), (t2y - t1y))
+
+            vetordeslocado = -vetor[1]*0.4, vetor[0]*0.4
+
+            final = (pmedio[0] + vetordeslocado[0], pmedio[1] + vetordeslocado[1])
+
+
+            number = self.number_of_transitions_beetween(t1, t2)
+
+            self.canvas.create_text(final[0], final[1]+30*(number-1), text=values, fill="#0000ff", font=("Arial", 24), tags="transition")
+            self.transitions_holder.append((t1, t2, values))
+            self.aux_add_transition = None
+            self.show_transitions(self.transitions_holder)
+
+
+            sec_window.destroy()
+            return
+
+        quitbutton = tk.Button(sec_window, text="OK", command=add_text_and_close)
+        quitbutton.pack()
+        
+    def is_any_transition_with(self, t1, t2, values):
+        for transition in self.transitions_holder:
+            if t1 == transition[0] and t2 == transition[1] and values == transition[2]:
+                return True
+        return False
+
+    def number_of_transitions_beetween(self, t1, t2):
+        count = 0
+        for transition in self.transitions_holder:
+            if t1 == transition[0] and t2 == transition[1]:
+                count += 1
+        return count
 
     def create_buttons(self):
         self.optionsHolder = tk.Frame(self.master, width=150, height=self._HEIGHT, bg="white", )
@@ -125,6 +205,8 @@ class Interface():
         self.RmTransition.pack()
         return
 
+
+
     def create_canvas(self, _width, _height):
         self.canvas = tk.Canvas(self.master, width=_width, height=_height)
         self.canvas.bind("<Button-1>", self.mouse_click_1)
@@ -132,7 +214,7 @@ class Interface():
         return
 
     def show_transition(self, transition):
-        t1, t2 = transition
+        t1, t2, _ = transition
         t1x, t1y, t1r = self.circles_holder[t1]
         t2x, t2y, t2r = self.circles_holder[t2]
 
@@ -147,7 +229,26 @@ class Interface():
         t2x += offsetx2
         t2y += offsety2
 
-        self.canvas.create_line(t1x, t1y, t2x, t2y, fill="#f11", width=2, tags="transition")
+        pmedio = ((t1x + t2x)/2, (t1y + t2y)/2)
+
+        vetor = ((t2x - t1x), (t2y - t1y))
+
+        vetordeslocado = -vetor[1]*0.4, vetor[0]*0.4
+
+        final = (pmedio[0] + vetordeslocado[0], pmedio[1] + vetordeslocado[1])
+
+
+
+
+
+
+        self.canvas.create_line(t1x, t1y, final[0], final[1], t2x, t2y, fill="#f11", width=2, smooth=1, tags="transition")
+        
+        
+        
+
+    
+        
         return
 
     def show_transitions(self, transitions):
@@ -166,7 +267,7 @@ class Interface():
         return
 
     def show_circles_at(self, circles):
-        if circles == []:
+        if circles is None:
             return
         i = 0
         for circle in circles:
